1a39c51c06ae39681d880cc88f6f43e0ab9eb040
xifan2333 <xifan233@163.com>
2026-03-05 09:01:53 +0800

    nvim: 禁用 neo-tree 插件

--- dotfiles/.config/nvim/lua/plugins/neo-tree.lua
+++ dotfiles/.config/nvim/lua/plugins/neo-tree.lua
@@ -1,11 +1,4 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
-  opts = {
-    filesystem = {
-      filtered_items = {
-        visible = true,
-        hide_gitignored = false,
-      },
-    },
-  },
+  enabled = false,
 }
