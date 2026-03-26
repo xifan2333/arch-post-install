0ec6d2b4033e7d8cddee40ee7bd6e8416660413e
xifan2333 <xifan233@163.com>
2026-03-26 19:53:28 +0800

    feat(nvim): add lazygit integration and xdg-open for binary files
    - Add lazygit.nvim with <leader>gg keybinding
    - Open non-image binary files with xdg-open via BufReadPre autocmd
    - Load image.nvim eagerly (lazy=false) to ensure it hijacks image files before xdg-open logic runs

--- dotfiles/.config/nvim/lua/plugins/git.lua
+++ dotfiles/.config/nvim/lua/plugins/git.lua
@@ -4,4 +4,12 @@ return {
     event = { "BufReadPre", "BufNewFile" },
     opts = {},
   },
+  {
+    "kdheepak/lazygit.nvim",
+    cmd = { "LazyGit", "LazyGitCurrentFile", "LazyGitFilterCurrentFile", "LazyGitFilter" },
+    dependencies = { "nvim-lua/plenary.nvim" },
+    keys = {
+      { "<leader>gg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+    },
+  },
 }
--- dotfiles/.config/nvim/lua/plugins/image.lua
+++ dotfiles/.config/nvim/lua/plugins/image.lua
@@ -1,6 +1,6 @@
 return {
   "3rd/image.nvim",
-  event = "BufReadPre",
+  lazy = false,
   opts = {
     backend = "kitty",
     hijack_file_patterns = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp", "*.avif" },
