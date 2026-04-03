6fd3124d5897f6eb76a7a0fe2df5114c6d04c026
xifan2333 <xifan233@163.com>
2026-04-03 16:59:13 +0800

    fix: remove manual Google UA override, let built-in quirks handle it (Firefox/145.0)

--- dotfiles/.config/qutebrowser/config.py
+++ dotfiles/.config/qutebrowser/config.py
@@ -1,10 +1,6 @@
 # qutebrowser配置
 config.load_autoconfig()
 
-# Google 账户使用 Firefox user agent 避免登录被阻止
-firefox_ua = 'Mozilla/5.0 (X11; Linux x86_64; rv:131.0) Gecko/20100101 Firefox/131.0'
-config.set('content.headers.user_agent', firefox_ua, 'https://accounts.google.com/*')
-config.set('content.headers.user_agent', firefox_ua, 'https://*.google.com/*')
 
 # 设置搜索引擎
 c.url.searchengines = {
