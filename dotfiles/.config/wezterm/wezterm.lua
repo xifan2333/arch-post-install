bbc3fbd33d7a4d2a51f90fb28621a5dcdf95e288
xifan2333 <xifan233@163.com>
2026-07-13 18:53:35 +0800

    wezterm: add Alt+[/] for window switching

--- dotfiles/.config/wezterm/wezterm.lua
+++ dotfiles/.config/wezterm/wezterm.lua
@@ -207,6 +207,18 @@ config.keys = {
     action = act.ActivateTabRelative(1),
   },
 
+  -- Non-leader window nav: Alt+[ / Alt+] 切窗口
+  {
+    key = "[",
+    mods = "ALT",
+    action = act.ActivateWindowRelative(-1),
+  },
+  {
+    key = "]",
+    mods = "ALT",
+    action = act.ActivateWindowRelative(1),
+  },
+
   -- Non-leader pane nav: Alt+hjkl + Alt+方向键 一键切面板，不用先按 C-a
   {
     key = "h",
