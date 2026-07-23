b316914c748bdf86499605f9941c775e24efc9fa
xifan2333 <xifan233@163.com>
2026-07-23 22:28:36 +0800

    fix: WezTerm corral → F12 (avoid input/ WM conflicts)

--- dotfiles/.config/wezterm/wezterm.lua
+++ dotfiles/.config/wezterm/wezterm.lua
@@ -131,7 +131,7 @@ config.check_for_updates = false
 config.disable_default_mouse_bindings = false
 
 config.keys = {
-  { key = 'f', mods = 'SUPER', action = wezterm.action.SpawnCommandInNewTab {
+  { key = 'F12', mods = 'NONE', action = wezterm.action.SpawnCommandInNewTab {
     args = { 'corral' },
   } },
 
