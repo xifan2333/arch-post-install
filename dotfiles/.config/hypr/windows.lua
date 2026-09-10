4de0b3e3e846c9c3b2de886cc138b07d70287192
稀饭 <xifan233@163.com>
2026-09-10 11:03:10 +0800

    refactor: remove unused hyprland configurations (#15) (#16)
    * chore: initialize draft pr for issue #15
    
    * refactor(hypr): remove hyprland configurations and dotfiles mapping (#15)
    
    * docs: clean up hyprland guidelines and references (#15)

--- dotfiles/.config/hypr/windows.lua
+++
@@ -1,7 +0,0 @@
--- Personal window rules on top of Omarchy defaults.
--- Current syntax: https://wiki.hypr.land/Configuring/Basics/Window-Rules/
-
--- Give WezTerm the same terminal treatment as Omarchy's stock terminals.
-o.window("org\\.wezfurlong\\.wezterm", { tag = "+terminal" })
-o.window("org\\.wezfurlong\\.wezterm", { tag = "-default-opacity" })
-o.window("org\\.wezfurlong\\.wezterm", { opacity = "0.985 0.96" })
